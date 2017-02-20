66eb803f2635460faf980945b5838cb3be3b6af6
Javier Laurenzano <jlaurenzano@altimetrik.com>
2017-02-20 14:22:43 -0300

    com

--- src/main.js
+++ src/main.js
@@ -4,15 +4,4 @@ import App from './App.vue'
 new Vue({
   el: '#app',
   render: h => h(App)
-});
-new Vue({
-  el: '#app-5',
-  data: {
-    message: 'Hello Vue.js!'
-  },
-  methods: {
-    reverseMessage: function () {
-      this.message = this.message.split('').reverse().join('')
-    }
-  }
-});
+})
