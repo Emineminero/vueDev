cd9882d1acfca4cf5753f2290903691c21abc2e3
Javier Laurenzano <jlaurenzano@altimetrik.com>
2017-02-20 14:29:02 -0300

    apsodas

--- src/main.js
+++ src/main.js
@@ -5,14 +5,3 @@ new Vue({
   el: '#app',
   render: h => h(App)
 });
-new Vue({
-  el: '#app-5',
-  data: {
-    message: 'Hello Vue.js!'
-  },
-  methods: {
-    reverseMessage: function () {
-      this.message = this.message.split('').reverse().join('')
-    }
-  }
-});
