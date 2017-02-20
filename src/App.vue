cb021727cb22b2cca7707cf7b2407e4acd9884c3
Javier Laurenzano <jlaurenzano@altimetrik.com>
2017-02-20 16:16:16 -0300

    ll

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,12 @@
       <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
     </ul>
     <h2>Ecosystem</h2>
-    <h1>MAIAMEEEEE</h1>
+    <ul>
+      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
+      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
+      <li><a href="https://github.com/vuejs/vueify" target="_blank">vueify</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+    </ul>
   </div>
 </template>
 
@@ -19,7 +24,7 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome tos Your App'
+      msg: 'Welcome to Your Vue.js App'
     }
   }
 }
@@ -34,26 +39,21 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-
 img {
   width: 200px;
   height: 200px;
 }
-
 h1, h2 {
   font-weight: normal;
 }
-
 ul {
   list-style-type: none;
   padding: 0;
 }
-
 li {
   display: inline-block;
   margin: 0 10px;
 }
-
 a {
   color: #42b983;
 }
